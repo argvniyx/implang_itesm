16d5a3ee2fc4be6a4e2bf1fe33f16404b4f8b659
Ricardo Vela <aricav96@gmail.com>
2021-04-17 10:11:03 -0500

    Fix project structure. Move our code to apps/
    Before, we had our own `app.py`, and so we did the
    application setup. However, now we are simply providing a
    page for a bigger app. Therefore we have moved our file to
    the apps directory.
    
    We will have to check how to make our use of styles agree with the
    whole page, and how to use assets considering we are no longer at
    the project root.

--- app.py
+++ app.py
@@ -1,36 +1,56 @@
-# -*- coding: utf-8 -*-
-
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-import dash_bootstrap_components as dbc
-import plotly.express as px
-import pandas as pd
+import dash_bootstrap_components as dbc 
+from dash.dependencies import Input, Output
 
-from components.header import header
-from components.neighborhood_section import neighborhood_section
-from components.section import section
-from components.curb_evaluation import curb_evaluation
-from components.overview_section import overview_section
 
-app = dash.Dash(__name__,
-                external_stylesheets=['assets/bootstrap.css'],
-                meta_tags=[
-                    {"name": "viewport", "content": "width=device-width, initial-scale=1"},
+app = dash.Dash(__name__, title='Instituto Municipal de Planeación y Gestión Urbana - IMPLANG', external_stylesheets=[dbc.themes.BOOTSTRAP],
+				meta_tags=[{'name': 'viewport',
+                            'content': 'width=device-width, initial-scale=1.0'}])
+
+
+
+server = app.server
+
+
+# Connect to app pages
+
+from apps import home, itesm
+
+
+# App Layout
+
+app.layout = dbc.Container([
+
+	dbc.NavbarSimple(
+		[
+
+        	dbc.Button('ITESM', href='/apps/itesm', color='light'),
+
+		],
+		brand='IMPLANG',
+		brand_href='/apps/home'
+	),
+
+	html.Div(id='page-content', children=[]),
+	dcc.Location(id='url', refresh=False)
+
 ])
 
-sections = [
-    neighborhood_section,
-    curb_evaluation
-]
 
-app.layout = dbc.Container(
-    children=[
-        header(),
-        overview_section(),
-        *[section(func) for func in sections]
-    ],
-    fluid=True)
+@app.callback(Output(component_id='page-content', component_property=
+					'children'),
+			[Input(component_id='url', component_property='pathname')])
+
+def display_page(pathname):
+	if pathname == '/apps/itesm':
+		return itesm.layout
+	else:
+		return home.layout
+
 
 if __name__ == '__main__':
-    app.run_server(debug=True, host="0.0.0.0")
+	app.run_server(debug=True)
+
+
--- apps/itesm.py
+++ apps/itesm.py
@@ -1,117 +1,36 @@
+# -*- coding: utf-8 -*-
+
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import dash_bootstrap_components as dbc
 import plotly.express as px
+import pandas as pd
 
-layout = html.Div([
+from components.header import header
+from components.neighborhood_section import neighborhood_section
+from components.section import section
+from components.curb_evaluation import curb_evaluation
+from components.overview_section import overview_section
 
-    ####################################### COMIENZA ESPACIO DE EDICIÓN #######################################
-
-    ## BANNER PRINCIPAL
-    
-    dbc.Row(
-        dbc.Col([
-            html.Img(src='../assets/imagen.png', style={'max-width':'100%', 'height':'auto'}),
-            html.H2('Ejemplo título banner',
-                style={'position': 'absolute', 'top': '50%', 'left': '50%',
-                'transform': 'translate(-50%, -50%)'})
-        ], style={'color': 'white', 'position': 'relative', 'text-align': 'center'})
-    ),
-
-    ## SECCIÓN 1
-    dbc.Container([
-        
-        ## Títutlo
-        dbc.Row(
-            dbc.Col(
-                html.H2('Ejemplo de título')
-            ), className='px-1 pt-4'
-        ),
-
-        ## Texto
-        dbc.Row(
-            dbc.Col(
-                html.H5('La bicicleta tiene enormes beneficios no sólo para la salud sino también para el medio ambiente, ya que se trata de un medio de transporte que favorece la movilidad sostenible.')  
-            ), className='px-1 py-4'
-        )
-
-    ]),
-
-
-    ## SECCION 2
-    dbc.Container([
-        # Título
-        dbc.Row(
-            dbc.Col(
-                html.H2('Otro ejemplo de título')
-                ),className='py-3', style={'background-color': 'black','color': 'white'}
-            ),
-
-        ## Texto
-        dbc.Row([
-            dbc.Col(
-                html.H5('La bicicleta tiene enormes beneficios no sólo para la salud sino también para el medio ambiente, ya que se trata de un medio de transporte que favorece la movilidad sostenible.'), lg=3, md=9, sm=4
-            ),
-            dbc.Col(
-                html.H5('La bicicleta tiene enormes beneficios no sólo para la salud sino también para el medio ambiente, ya que se trata de un medio de transporte que favorece la movilidad sostenible.'), lg=9, md=3, sm=8
-            ), 
-        ],className='py-3')
-
-    ]),
-
-    ######################################## TERMINA ESPACIO DE EDICIÓN ########################################
-
-    # Footer
-    dbc.Container([
-    
-        dbc.Row(
-            dbc.Col(
-              html.H6('Envíanos un correo a implang@sanpedro.gob.mx')  
-            ), className='px-1 pt-4'
-        ),
-
-        dbc.Row(
-            dbc.Col([
-                html.A(
-                    html.Img(src='../assets/instagram.png', style={'max-width':'85px', 'height':'34px'}),
-                    href='https://www.instagram.com/implang_spgg/', target='blank'
-                ),
-
-                html.A(
-                    html.Img(src='../assets/facebook.png', style={'max-width':'85px', 'height':'34px'}),
-                    href='https://www.facebook.com/implangspgg', target='blank', className='pl-3'
-                ),
-
-                html.A(
-                    html.Img(src='../assets/twitter.png', style={'max-width':'85px', 'height':'34px'}),
-                    href='https://twitter.com/implang_spgg', target='blank', className='pl-3'
-                ),
-
-                html.A(
-                    html.Img(src='../assets/youtube.png',style={'max-width':'85px', 'height':'34px'}),
-                    href='https://www.youtube.com/channel/UCZwYFPh0dHnKhXqzaxlaqNg', target='blank',
-                    className='pl-3'
-                )
-            ]), className='px-1 py-4'
-        )
-        
-    ]),
-
-    dbc.Container([
-
-       dbc.Row(
-            dbc.Col(
-                html.H6('Instituto Municipal de Planeación y Gestión Urbana')
-            ), className='px-1 pt-3'
-        ),
-
-        dbc.Row(
-            dbc.Col(
-                html.H6('San Pedro Garza García, Nuevo León, México')
-            ), className='px-1 py-3'
-        )
-        
-    ], style={'background-color': 'black','color': 'white'}
-    )
+app = dash.Dash(__name__,
+                external_stylesheets=['assets/bootstrap.css'],
+                meta_tags=[
+                    {"name": "viewport", "content": "width=device-width, initial-scale=1"},
 ])
+
+sections = [
+    neighborhood_section,
+    curb_evaluation
+]
+
+layout = dbc.Container(
+    children=[
+        header(),
+        overview_section(),
+        *[section(func) for func in sections]
+    ],
+    fluid=True)
+
+if __name__ == '__main__':
+    app.run_server(debug=True, host="0.0.0.0")
