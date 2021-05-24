760ab2e46da07c59f9d4cf1bf4f13b5f0d0864d3
Ricardo Vela <aricav96@gmail.com>
2021-05-24 16:59:16 -0500

    Scroll hijack work. Key press navigation
    TODO:
    - Cleanup
    - Mutex the keypresses. As they are now, it is possible to press
    the arrow keys in quick succession, which leaves the page in a weird
    position between sections

--- assets/scroll.js
+++ assets/scroll.js
@@ -4,7 +4,6 @@ document.documentElement.style.setProperty('--vh', `${vh}px`);
 window.dash_clientside = Object.assign({}, window.dash_clientside, {
     clientside: {
         scrollSection: function() {
-            console.log("in scrollSection")
             direction =
                 JSON.parse(
                     dash_clientside
@@ -17,7 +16,7 @@ window.dash_clientside = Object.assign({}, window.dash_clientside, {
             factor = direction == "up" ? -1 : 1;
 
             window.scrollBy({
-                top: factor * document.documentElement.clientHeight,
+                top: factor * window.innerHeight,
                 behavior: "smooth"
             });
         },
@@ -30,7 +29,25 @@ window.dash_clientside = Object.assign({}, window.dash_clientside, {
     }
 });
 
+const scroll = (factor) => {
+    window.scrollBy({
+        top: factor * window.innerHeight,
+        behavior: "smooth"
+    });
+}
+
 window.addEventListener('resize', () => {
     let vh = window.innerHeight * 0.01;
     document.documentElement.style.setProperty('--vh', `${vh}px`);
+})
+
+document.addEventListener('keydown', async (event) => {
+    const keyName = event.key;
+
+    if(keyName === 'ArrowDown') {
+        scroll(1);
+    }
+    else if(keyName === 'ArrowUp') {
+        scroll(-1);
+    }
 })
--- implang_utils/components/overview_section.py
+++ implang_utils/components/overview_section.py
@@ -8,15 +8,52 @@ import dash_html_components as html
 from implang_utils.data.dataframe import df_points
 from implang_utils.components.utils import highlight
 
+label_translations = {
+    "NoSidewalk": "No hay banqueta",
+    "Obstacle": "Obstáculo",
+    "SurfaceProblem": "Problema de superficie",
+    "NoCurbRamp": "Sin Rampa",
+    "CurbRamp": "Con Rampa",
+    "Occlusion": "Obstrucción de vista",
+    "Other": "Otro"
+}
+
+positive_labels = ["CurbRamp"]
+neutral_labels = ["Other"]
+negative_labels = [
+    label for label in list(label_translations.keys())
+    if label not in positive_labels + neutral_labels
+]
+
 total_defects = df_points.shape[0]
 defect_counts = df_points["label_type"].value_counts()
 
-cards = dbc.CardDeck(
+positive_counts = len([p for p in defect_counts if p in positive_labels])
+neutral_counts = len([p for p in defect_counts if p in neutral_labels])
+negative_counts = len([p for p in defect_counts if p in negative_labels])
+
+
+category_cards = dbc.CardDeck(
     [
         dbc.Card(
             dbc.CardBody(
                 [
-                    html.H5(defect, className="card-title"),
+                    html.H5(label_translations[defect], className="card-title"),
+                    html.P(defect_counts[defect], className="card-text")
+                ]
+            ),
+            className="mt-3 md-3"
+        )
+        for defect in defect_counts.keys()
+    ],
+)
+
+label_cards = dbc.CardDeck(
+    [
+        dbc.Card(
+            dbc.CardBody(
+                [
+                    html.H5(label_translations[defect], className="card-title"),
                     html.P(defect_counts[defect], className="card-text")
                 ]
             ),
@@ -31,20 +68,25 @@ def overview_section():
     "DB Component<Container>"
 
     return dbc.Card(
-       [
-           dbc.CardBody(
-               [
-                   html.H1("En San Pedro", className="card-title"),
-                   html.H5(
-                       [
-                           "Hay un total de ",
-                           highlight(
-                               f'{total_defects} defectos en banquetas'
-                           )
-                       ]
-                   ),
-                   cards
-               ],
-           ),
-       ]
+        [
+            dbc.CardBody(
+                [
+                    html.H1("En San Pedro", className="card-title"),
+                    html.H5(
+                        [
+                            "Se han hecho un total de ",
+                            highlight(
+                                html.Span(
+                                    f'{total_defects} observaciones de banquetas:',
+                                    className="text-light"
+                                ),
+                                "green"
+                            ),
+                        ]
+                    ),
+                    label_cards,
+                    category_cards
+                ],
+            ),
+        ]
     )
