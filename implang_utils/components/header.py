dea617f6154c3cccffa291e3ba2c00150c88b7b9
Esteban Torres <esteban.t97@gmail.com>
2021-06-04 15:41:59 -0500

    changes in styles for the header.py

--- implang_utils/components/header.py
+++ implang_utils/components/header.py
@@ -14,15 +14,18 @@ def header():
         [
             html.H1(
                 "Movilidad IMPLANG",
-                className="display-2 d-none d-md-block"
+                className="display-2 d-none d-md-block",
+                style={"text-align" : "center"}
             ),
             html.H1(
                 "Movilidad IMPLANG",
                 className="d-block d-md-none"
             ),
-            html.H2("El estado de las banquetas de San Pedro Garza García, Monterrey"),
-            html.H3("Según el INEGI en Nuevo León existen 220 mil 206 personas con discapacidad lo que representa al 3.80 por ciento de la población"),
-            html.H3("Es decir, 38 personas de cada mil sufren una discapacidad")
+            html.Div([
+                html.H2("El estado de las banquetas de San Pedro Garza García, Monterrey"),
+                html.H3("Según el INEGI en Nuevo León existen 220 mil 206 personas con discapacidad lo que representa al 3.80 por ciento de la población"),
+                html.H3("Es decir, 38 personas de cada mil sufren una discapacidad")            
+            ], style={"text-align" : "center"})
         ],
         className="rounded-0 h-100 m-0",
         style={
@@ -30,5 +33,8 @@ def header():
             "backgroundPosition": "center",
             "backgroundSize": "cover",
             "backgroundRepeat": "no-repeat",
+            "display": "flex",
+            "flex-flow": "column",
+            "justify-content": "center"
         }
     )
