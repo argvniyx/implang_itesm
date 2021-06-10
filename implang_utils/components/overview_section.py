5c2dfcbe361ff70bc72990be46cc484514a5cabc
Ricardo Vela <aricav96@gmail.com>
2021-06-10 13:19:09 -0500

    Last minute neighborhood replica of report

--- implang_utils/components/overview_section.py
+++ implang_utils/components/overview_section.py
@@ -5,7 +5,6 @@ defectos/observaciones de banquetas
 """
 import dash_bootstrap_components as dbc
 import dash_html_components as html
-from functools import reduce
 from implang_utils.data.dataframe import df_points
 from implang_utils.components.utils import highlight
 from implang_utils.components.count_report import observation_types
